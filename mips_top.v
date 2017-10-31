20eb5ed0b5e8c7dfd56f8513549b367a2b21bfa8
moffittengine <moffitt.engine@gmail.com>
2017-10-30 21:47:48 -0700

    test files synth ran no critical errors

--- mips_top.v
+++ mips_top.v
@@ -4,4 +4,37 @@ module mips_top
     mips      MIPS (clk, rst, instr, rd_dm, we_dm, pc_current, alu_out, wd_dm);
     imem #(32)IMEM (pc_current[7:2], instr);
     dmem #(32)DMEM (clk, we_dm, alu_out[5:0], wd_dm, rd_dm);
+    
+    
+    CPzero dut1(
+        .clk (0), 
+        .rst (0), 
+        .we1 (0), 
+    	.alu_trap(0), 
+    	.addr(0), 
+    	.interrupt(0), 
+    	.wd  (0), 
+    	.pcp4(0), 
+        .exl (), 
+        .iv  (), 
+     	.rd1 ()
+     	);
+    
+    aes_block dut2(
+        // Clock and reset.
+        .clk    (0),
+        .reset_n(0),
+       
+        // Control.
+        .cs(0),
+        .we(0),
+
+        // Data ports.
+        .address   (0),
+        .INT       (),
+
+        .write_block(),
+        .read_block ()
+        );
+    
 endmodule
