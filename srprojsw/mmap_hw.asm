a9945cb5c05b33e4441329bc1134db4e0a9ea5ce
Ryan Lucus <ryan.lucus@gmail.com>
2017-11-28 16:44:07 -0800

    mmap_hw update

--- srprojsw/mmap_hw.asm
+++ srprojsw/mmap_hw.asm
@@ -9,16 +9,18 @@
 	.eqv	ISRS3	0x7EEC # $t0 - Temporary register 0
 
 	##Scheduler Storage Locations
-	.eqv	PCB_BASE0	0x7F00
-	.eqv	PCB_BASE1	0x7F20
-	.eqv	PCB_BASE2	0x7F40
-	.eqv	PCB_BASE3	0x7F60
-	.eqv	PCB_BASE4	0x7F80
-	.eqv	PCB_BASE5	0x7FA0
-	.eqv	PCB_BASE6	0x7FC0
+	.eqv	PCB_BASE0	0x6000
+	.eqv	PCB_BASE1	0x6100
+	.eqv	PCB_BASE2	0x6200
+	.eqv	PCB_BASE3	0x6300
+	.eqv	PCB_BASE4	0x6400
+	.eqv	PCB_BASE5	0x6500
+	.eqv	PCB_BASE6	0x6600
 	.eqv	SCHED_STATUS	0x7FE0
+	.eqv	SCHED_LAST_TASK 0x7FE4
 	.eqv	iMEM_BASE_MASK	0x1000
-	.eqv	dMEM_END_MASK	0x0FFF
+	.eqv	dMEM_END_MASK	0x00FF
+	.eqv	dMEM_OFFSET_MASK	0x0008
 
 	.eqv	SCHEDS0	0x7FF0 # CP0/R14 - Interrupted Instruction
 	.eqv	SCHEDS1	0x7FF4 # $ra - Return address of interrupted proc
